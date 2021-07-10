ea702dbdd21e884e4f34649c1e747d78b9d5cf8f
Farhad Ariamaram <farhad.ariamaram@gmail.com>
2021-07-10 10:12:06 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-self.addEventListener('install',function(event){
-  console.log('Service Worker Installing ...',event);
+self.addEventListener('install', function (event) {
+    console.log('[SW] Installing SW...', event);
 });
 
-self.addEventListener('activate',function(event){
-  console.log('Service Worker Activating ....',event);
+self.addEventListener('activate', function (event) {
+    console.log('[SW] activating SW...', event);
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('[SW] fetching SW ...', event);
 });
